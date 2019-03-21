283c5b8e9f776706e6173d38ed274269170fa86a
levin <3259963@qq.com>
2019-03-21 22:29:57 +0800

    用网上的教程在 hdfs 系统上创建了目录

--- src/main/java/org/training/hadoop/hdfs/HdfsExample.java
+++ src/main/java/org/training/hadoop/hdfs/HdfsExample.java
@@ -4,19 +4,29 @@ import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 
+import java.net.URI;
+
 public class HdfsExample {
 
   public static void testMkdirPath(String path) throws Exception {
     FileSystem fs = null;
     try {
-      System.out.println("Creating " + path + " on hdfs...");
-      Configuration conf = new Configuration();
-      // First create a new directory with mkdirs
-      Path myPath = new Path(path);
-      fs = myPath.getFileSystem(conf);
+//      System.out.println("Creating " + path + " on hdfs...");
+//      Configuration conf = new Configuration();
+//      // First create a new directory with mkdirs
+//      Path myPath = new Path(path);
+//      fs = myPath.getFileSystem(conf);
+//
+//      fs.mkdirs(myPath);
+//      System.out.println("Create " + path + " on hdfs successfully.");
 
-      fs.mkdirs(myPath);
-      System.out.println("Create " + path + " on hdfs successfully.");
+      String uri = "hdfs://localhost:9000/mydemodir";
+      Configuration conf = new Configuration();
+      conf.set("fs.hdfs.impl", org.apache.hadoop.hdfs.DistributedFileSystem.class.getName());
+      conf.set("fs.file.imp", org.apache.hadoop.fs.LocalFileSystem.class.getName());
+      fs = FileSystem.get(URI.create(uri), conf);
+      boolean yesorno = fs.mkdirs(new Path(uri));
+      System.out.println("Status of given directory is" + yesorno);
     } catch (Exception e) {
       System.out.println("Exception:" + e);
     } finally {
