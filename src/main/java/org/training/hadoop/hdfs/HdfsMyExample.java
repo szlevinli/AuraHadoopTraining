ddbc5bad49a4118decc9eae3035af3109d4538e2
levin <3259963@qq.com>
2019-03-22 20:29:55 +0800

    完成在 hdfs 上创建目录，读些删除文件

--- src/main/java/org/training/hadoop/hdfs/HdfsMyExample.java
+++ src/main/java/org/training/hadoop/hdfs/HdfsMyExample.java
@@ -21,53 +21,76 @@ public class HdfsMyExample {
     }
 
     public static void runAll() throws Exception {
-        String hdfsUri = "hdfs://192.168.12.158:9000";
+        String hdfsUri = "hdfs://10.0.0.114:9000";
         String path = "/test";
         String fileName = "hello.txt";
         String fileContent = "hello world!";
 
-        // ===== Init HDFS File System Object
-        System.out.println("start init HDFS file system object...");
-        Configuration conf = new Configuration();
-        // Set FileSystem URI
-        conf.set("fs.defaultFS", hdfsUri);
-        // Because Maven
-        conf.set("fs.hdfs.impl", org.apache.hadoop.hdfs.DistributedFileSystem.class.getName());
-        conf.set("fs.file.impl", org.apache.hadoop.fs.LocalFileSystem.class.getName());
-        // Set Hadoop User
-        System.setProperty("HADOOP_USER_NAME", "bigdata");
-        System.setProperty("hadoop.home.dir", "/");
-        // Get HDFS FileSystem Object
-        FileSystem fs = FileSystem.get(URI.create(hdfsUri), conf);
+        FileSystem fs = null;
+        FSDataInputStream fIn = null;
+        FSDataOutputStream fOut = null;
 
-        // ===== Create Folder if not exists
-        System.out.println("start create folder...");
-        Path workingPath = fs.getWorkingDirectory();
-        Path newFolderPath = new Path(path);
-        if (!fs.exists(newFolderPath)) {
-            // create folder
-            fs.mkdirs(newFolderPath);
+        try {
+            // ===== Init HDFS File System Object
+            System.out.println("start init HDFS file system object...");
+            Configuration conf = new Configuration();
+            // Set FileSystem URI
+            conf.set("fs.defaultFS", hdfsUri);
+            // Because Maven
+            conf.set("fs.hdfs.impl", org.apache.hadoop.hdfs.DistributedFileSystem.class.getName());
+            conf.set("fs.file.impl", org.apache.hadoop.fs.LocalFileSystem.class.getName());
+            // Set Hadoop User
+            System.setProperty("HADOOP_USER_NAME", "bigdata");
+            System.setProperty("hadoop.home.dir", "/");
+            // Get HDFS FileSystem Object
+            fs = FileSystem.get(URI.create(hdfsUri), conf);
+
+            // ===== Create Folder if not exists
+            System.out.println("start create folder...");
+            Path workingPath = fs.getWorkingDirectory();
+            Path newFolderPath = new Path(path);
+            if (!fs.exists(newFolderPath)) {
+                // create folder
+                fs.mkdirs(newFolderPath);
+            }
+
+            // ===== Write file
+            System.out.println("start write file...");
+            // create path
+            Path hdfsWritePath = new Path(newFolderPath + "/" + fileName);
+            // init output stream
+            fOut = fs.create(hdfsWritePath);
+            // classical output stream usage
+            fOut.writeBytes(fileContent);
+            fOut.close();
+
+            // ===== Read file
+            System.out.println("start read file...");
+            // create path
+            Path hdfsReadPath = new Path(newFolderPath + "/" + fileName);
+            // init input stream
+            fIn = fs.open(hdfsReadPath);
+            // classical input stream usage
+            String content = IOUtils.toString(fIn, "UTF-8");
+            System.out.println("file contents is " + content);
+            fIn.close();
+
+            // ===== Delete file
+            System.out.println("start delete file...");
+            // create path
+            Path hdfsDeletePath = new Path(newFolderPath + "/" + fileName);
+            boolean isRecursive = true;
+            fs.delete(hdfsDeletePath, isRecursive);
+        } catch (Exception e) {
+            System.out.println("Exception:" + e);
+        } finally {
+            try {
+                if (fs != null) fs.close();
+                if (fIn != null) fIn.close();
+                if (fOut != null) fOut.close();
+            } catch (Exception ignored) {
+                // nothing to do.
+            }
         }
-
-        // ===== Write file
-        System.out.println("start write file...");
-        // create path
-        Path hdfsWritePath = new Path(newFolderPath + "/" + fileName);
-        // init output stream
-        FSDataOutputStream fOut = fs.create(hdfsWritePath);
-        // classical output stream usage
-        fOut.writeBytes(fileContent);
-        fOut.close();
-
-        // ===== Read file
-        System.out.println("start read file...");
-        // create path
-        Path hdfsReadPath = new Path(newFolderPath + "/" + fileName);
-        // init input stream
-        FSDataInputStream fIn = fs.open(hdfsReadPath);
-        // classical input stream usage
-        String content = IOUtils.toString(fIn, "UTF-8");
-        fIn.close();
-        fs.close();
     }
 }
